ef42ec3f0599cb38b7d470a5359bcc0dcf10c0c9
Valentin Bas <vbas@persistant.fr>
2016-05-11 11:55:09 +0200

    push project

---
+++ Assets/Scripts/YokaiJSONLoader.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+using System.Text;
+
+public class YokaiJSONLoader : MonoBehaviour
+{
+	public string filename = "YokaiList.json";
+
+	public JSONNode YokaiList;
+
+	void Start()
+	{
+		YokaiList = null;
+	}
+
+	public void Load()
+	{
+		YokaiList = null;
+		StartCoroutine(LoadJSONFile());
+	}
+
+	private IEnumerator LoadJSONFile()
+	{
+		WWW data = new WWW("file:///" + Application.streamingAssetsPath + "/" + filename);
+		yield return data;
+
+		if (string.IsNullOrEmpty(data.error))
+		{
+			OnFileLoaded(Encoding.Default.GetString(data.bytes));
+		}
+	}
+
+	private void OnFileLoaded(string data)
+	{
+		var N = JSON.Parse(data);
+		YokaiList = N["data"]["yokaiList"];
+	}
+}
---
+++ Assets/Scripts/YokaiScrollList.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YokaiScrollList : UIList<ScrollCellTest>
+{
+	public YokaiJSONLoader Loader;
+
+	private bool Initialized;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Initialized = false;
+		if (Loader != null)
+			Loader.Load();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!Initialized && Loader != null && Loader.YokaiList != null)
+		{
+			CreateList();
+			Initialized = true;
+		}
+	}
+
+	public void CreateList()
+	{
+		int cells = Loader.YokaiList.Count;
+
+		if (cells > 0)
+		{
+			ClearAllCells();
+			Refresh();
+		}
+	}
+
+	#region implemented abstract members of UIList
+
+	public override int NumberOfCells()
+	{
+		if (Loader.YokaiList == null)
+			return 0;
+		return Loader.YokaiList.Count;
+	}
+
+	public override void UpdateCell(int index, ScrollCellTest cell)
+	{
+		cell.cellLabel.text = Loader.YokaiList[index]["name-fr"];
+	}
+
+	#endregion
+}
